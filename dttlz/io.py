502ef8de42307846867a2dccfeda6a467154e1a7
asciimoo <asciimoo@gmail.com>
2013-09-17 21:07:26 +0200

    [enh][doc] function descriptions

--- dttlz/io.py
+++ dttlz/io.py
@@ -1,3 +1,16 @@
+# -*- coding: utf-8 -*-
+
+"""
+dttlz.io
+~~~~~~~~
+
+This module implements the dttlz io functions.
+
+:copyright: (c) 2013 by Adam Tauber.
+:license: AGPL, see LICENSE for more details.
+
+"""
+
 import json as _json
 import pickle as _pickle
 import csv as _csv
@@ -5,41 +18,60 @@ from StringIO import StringIO
 
 
 def load_json(json_string, **kwargs):
+    """loads a json string to python object
+
+    kwargs passed to json.loads"""
     return _json.loads(json_string, **kwargs)
 
 
 def load_json_file(json_filename, **kwargs):
+    """loads a json file to python object
+
+    kwargs passed to json.load"""
     with open(json_filename, 'r') as json_file:
         return _json.load(json_file, **kwargs)
 
 
 def dump_json(obj, **kwargs):
+    """python object to json string
+
+    kwargs passed to json.dumps"""
     return _json.dumps(obj, **kwargs)
 
 
 def dump_json_file(obj, filename, **kwargs):
+    """python object to json file
+
+    kwargs passed to json.dump"""
     with open(filename, 'wb') as outfile:
         return _json.dump(obj, outfile, **kwargs)
 
 
 def load_pickle(pickle_string):
+    """loads a pickle string to python object"""
     return _pickle.loads(pickle_string)
 
 
 def load_pickle_file(pickle_filename):
+    """loads a pickle file to python object"""
     with open(pickle_filename, 'rb') as pickle_file:
         return _pickle.load(pickle_file)
 
 def dump_pickle(obj):
+    """python object to pickle string"""
     return _pickle.dumps(obj)
 
 
 def dump_pickle_file(obj, filename):
+    """python object to pickle file"""
     with open(filename, 'wb') as outfile:
         return _pickle.dump(obj, outfile)
 
 
-def parse_csv(fp, delimiter=',', quotechar='"', header=True, typedict=None):
+def parse_csv(fp, delimiter=',', quotechar='"', typedict=None):
+    """parses csv file with DictReader
+
+    typedict defines row types (format: {'rowname': type})"""
     parser = _csv.DictReader(fp, delimiter=delimiter, quotechar=quotechar)
     data = []
     for x in parser:
@@ -50,6 +82,9 @@ def parse_csv(fp, delimiter=',', quotechar='"', header=True, typedict=None):
     return data
 
 def load_csv(csv_string, **kwargs):
+    """loads a csv string to python dict
+
+    kwargs passed to parse_csv"""
     csv_stringio = StringIO()
     csv_stringio.write(csv_string)
     csv_stringio.seek(0)
@@ -57,11 +92,15 @@ def load_csv(csv_string, **kwargs):
 
 
 def load_csv_file(csv_filename, **kwargs):
+    """loads a csv file to python dict
+
+    kwargs passed to parse_csv"""
     with open(csv_filename, 'rb') as csv_file:
         return parse_csv(csv_file, **kwargs)
 
 
 def dump_csv_file(obj, filename, **kwargs):
+    """python list/dict to csv file"""
     with open(filename, 'wb') as outfile:
         writer = _csv.DictWriter(outfile, **kwargs)
         if type(obj) == list:
@@ -72,6 +111,7 @@ def dump_csv_file(obj, filename, **kwargs):
                 writer.writerow(data)
 
 def dump_csv(obj, **kwargs):
+    """python list/dict to csv string"""
     out = StringIO()
     dump_csv_file(obj, out, **kwargs)
     out.seek(0)
